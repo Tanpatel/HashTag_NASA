cb73f6cac779c405aedab177644f170ddabe7eed
Tanpatel <101955781+Tanpatel@users.noreply.github.com>
2024-10-05 11:05:08 -0500

    Earth's Position

--- Sun/index.js
+++ Sun/index.js
@@ -32,7 +32,7 @@ const material = new THREE.MeshPhongMaterial({
 // material.map.colorSpace = THREE.SRGBColorSpace;
 const earthMesh = new THREE.Mesh(geometry, material);
 earthGroup.add(earthMesh);
-
+earthGroup.position.x = -5;
 const lightsMat = new THREE.MeshBasicMaterial({
   map: loader.load("./textures/sun.png"),
   blending: THREE.AdditiveBlending,
